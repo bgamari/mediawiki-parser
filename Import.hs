b6b4d378c7187fe7e1d4ef392460d5fdec8cf55a
Laura Dietz (wifo32) <dietz@informatik.uni-mannheim.de>
2016-07-03 12:26:32 +0200

    Fix UTF8 handling

--- Import.hs
+++ Import.hs
@@ -14,6 +14,7 @@ import Data.Foldable
 import Data.Monoid
 import Control.Parallel.Strategies
 import qualified Data.HashMap.Strict as HM
+import qualified Data.HashSet as HS
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Builder as BB
@@ -25,6 +26,7 @@ import qualified Data.Text.Lazy.Builder as TB
 import           Data.Text (Text)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL
+import qualified Data.Text.Lazy.Encoding as TLE
 
 import Control.Concurrent.Async
 import Pipes
@@ -47,14 +49,19 @@ main = do
           $ withStrategy (parBuffer 80 rseq)
           [ [ (ParseDump.docTitle doc, linkTarget, linkNamespace, linkAnchor)
             | Link{..} <- docLinks (map snd namespaces) doc
-            , not ("http://" `BS.isPrefixOf` linkTarget)
-            , not ("https://" `BS.isPrefixOf` linkTarget)
+            , not ("http://" `T.isPrefixOf` linkTarget)
+            , not ("https://" `T.isPrefixOf` linkTarget)
             ]
           | doc <- docs
           ]
  
     print namespaces
-    let showLink (a,b,c,d) = mconcat $ intersperse (BB.char8 '\t') [escape a, escape b, maybe "" escape c, escape d]
+    let showLink (src, dest, ns, anchor) = mconcat $ intersperse (BB.char8 '\t')
+            $ map TLE.encodeUtf8Builder
+            [ escape $ TE.decodeUtf8 src
+            , escape dest
+            , maybe "" escape ns
+            , escape anchor]
     --BSL.writeFile "links.out" $ BB.toLazyByteString $ mconcat $ intersperse (BB.char8 '\n') $ map showLink links
 
     let ci = defaultConnectInfo { connectHost = "localhost"
@@ -78,31 +85,28 @@ main = do
     wait writer
     return ()
 
-data Link = Link { linkTarget :: !ByteString
-                 , linkNamespace :: !(Maybe ByteString)
-                 , linkAnchor :: !ByteString
+data Link = Link { linkTarget :: !T.Text
+                 , linkNamespace :: !(Maybe T.Text)
+                 , linkAnchor :: !TL.Text
                  }
           deriving (Show)
 
-escape :: ByteString -> BB.Builder
-escape = go
+escape :: Text -> TL.Text
+escape = TB.toLazyText . go
   where
     go bs
-      | BS.null bs = mempty
-      | '\\' <- BS.head bs = "\\\\" <> go (BS.tail bs)
-      | '\t' <- BS.head bs = "\\t" <> go (BS.tail bs)
-      | '\n' <- BS.head bs = " " <> go (BS.tail bs)
+      | T.null bs = mempty
+      | '\\' <- T.head bs = "\\\\" <> go (T.tail bs)
+      | '\t' <- T.head bs = "\\t" <> go (T.tail bs)
+      | '\n' <- T.head bs = " " <> go (T.tail bs)
       | otherwise =
-        let (taken, rest) = BS.span (not . badChar) bs
-        in BB.byteString taken <> go rest
+        let (taken, rest) = T.span (not . badChar) bs
+        in TB.fromText taken <> go rest
       where
         badChar '\t' = True
         badChar '\n' = True
         badChar '\\' = True
         badChar _    = False
-
-stripWhitespace :: BS.ByteString -> BS.ByteString
-stripWhitespace = fst . BS.spanEnd isSpace . BS.dropWhile isSpace
 
 docLinks :: [Namespace] -> WikiDoc -> [Link]
 docLinks namespaces doc = 
@@ -110,26 +114,28 @@ docLinks namespaces doc =
       Success doc' -> foldMap findLinks doc'
       Failure err  -> trace ("dropped "++show (ParseDump.docTitle doc)++"\n"++show err) []  -- error $ show err
   where
-    namespaceNames = [ map toLower $ BS.unpack name | Namespace name <- namespaces ]
-    findLinks (InternalLink (PageName name) body) =
-      let (page, namespace)
-            | Just page <- ":" `BS.stripPrefix` name = (page, Nothing)
+    namespaceNames = HS.fromList [ T.toCaseFold $ TE.decodeUtf8 name
+                                 | Namespace name <- namespaces ]
+    findLinks (InternalLink (PageName name') body) =
+      let name = TE.decodeUtf8 name'
+          (page, namespace)
+            | Just page <- ":" `T.stripPrefix` name = (page, Nothing)
 
-            | (ns, page) <- BS.span (/= ':') name
-            , map toLower (BS.unpack ns) `elem` namespaceNames
-            , not (BS.null page)
-            = (stripWhitespace $ BS.tail page, Just $ stripWhitespace ns)
+            | (ns, page) <- T.span (/= ':') name
+            , T.toCaseFold ns `HS.member` namespaceNames
+            , not (T.null page)
+            = (T.strip $ T.tail page, Just $ T.strip ns)
 
             | otherwise 
             = (name, Nothing)
-      in [Link { linkAnchor = foldMap getText body
+      in [Link { linkAnchor = TLE.decodeUtf8 $ BB.toLazyByteString $ foldMap getText body
                , linkNamespace = namespace
                , linkTarget = page
                }]
     findLinks _ = []
 
-    getText :: Doc -> ByteString
-    getText (Text s)        = s
+    getText :: Doc -> BB.Builder
+    getText (Text s)        = BB.byteString s
     --getText (Bold s)        = s
     --getText (Italic s)      = s
     --getText (BoldItalic s)  = s
